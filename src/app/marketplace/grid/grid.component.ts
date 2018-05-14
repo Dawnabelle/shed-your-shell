3684126a5451cb14428684d923cefbc7b3033dff
Brenda Franco and Sean McDermott <brenda@>
2018-05-14 11:16:24 -0700

    Inject item service into marketplace and grid.

--- src/app/marketplace/grid/grid.component.ts
+++ src/app/marketplace/grid/grid.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemModel } from '../../models/item.model';
+import { ItemsService } from '../../items.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.css']
+  styleUrls: ['./grid.component.css'],
+  providers: [ItemsService]
 })
 export class GridComponent implements OnInit {
 
-  constructor() { }
+  items: FirebaseListObservable<any[]>;
+
+  constructor(private itemsService: ItemsService ) { }
 
   ngOnInit() {
+    this.items = this.itemsService.getItems();
   }
 
 }
