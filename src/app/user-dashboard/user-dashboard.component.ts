4360093e46569b89f8c3cbedb44b2ce55149393a
Guest User <Guest@Epicodus-6A.local>
2018-05-14 11:30:47 -0700

    add password section

--- src/app/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  goToMarketplace(clickedButton){
+    this.router.navigate(['marketplace']);
+  }
   ngOnInit() {
+
   }
 
 }
