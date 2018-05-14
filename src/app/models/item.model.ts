c67ea29bae88d49ce0909c59fccb43d8a3246ad5
Guest User <Guest@Epicodus-6A.local>
2018-05-14 16:19:02 -0700

    add working radio button inputs for uploading garment and change to model

--- src/app/models/item.model.ts
+++ src/app/models/item.model.ts
@@ -1,10 +1,13 @@
 export class ItemModel {
+  availability: boolean;
   constructor(
+    public userName: string,
     public type: string,
     public size: string,
     public colors: Array<string>,
     public cut: string,
-    public availability: boolean,
     public condition: string,
-    public userName: string){}
+    ){
+      this.availability=true;
+    }
 }
--- src/app/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ItemsService } from '../items.service';
+import { ItemModel } from './../models/item.model';
+import { FirebaseListObservable } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './user-dashboard.component.html',
-  styleUrls: ['./user-dashboard.component.css']
+  styleUrls: ['./user-dashboard.component.css'],
+  providers: [ItemsService]
 })
 export class UserDashboardComponent implements OnInit {
+  items: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
+  newItemType: string = "top";
+  newItemSize: string = "xs";
+  newItemColors: string = "red";
+  newItemCut: string = "masc";
+  newItemCondition: string = "new";
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private itemsService: ItemsService) { }
 
   goToMarketplace(clickedButton){
     this.router.navigate(['marketplace']);
@@ -16,5 +28,9 @@ export class UserDashboardComponent implements OnInit {
   ngOnInit() {
 
   }
-
+  submitForm(userName: string, type, size: string, colors: string, cut: string, condition: string){
+    var newItem: ItemModel = new ItemModel(userName, this.newItemType, this.newItemSize, this.newItemColors, this.newItemCut, this.newItemCondition);
+    console.log(newItem);
+    // this.itemsService.addItem(newItem);
+  }
 }
