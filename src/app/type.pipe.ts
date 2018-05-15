3976d7c4a7c792ab6dd336d2b404427d6cf21f0d
Brenda Franco and Sean McDermott <brenda+sean@>
2018-05-15 09:56:17 -0700

    Refactor type pipe again.

--- src/app/type.pipe.ts
+++ src/app/type.pipe.ts
@@ -10,80 +10,19 @@ import { ItemModel } from './models/item.model';
 export class TypePipe implements PipeTransform {
     transform(input: ItemModel[], desiredType) {
     let output: ItemModel[] = [];
-    let types: Array<string> = ["top", "bottom", "one-piece", "accessory", "dress"];
+    const types: Array<string> = ["top", "bottom", "one-piece", "accessory", "dress"];
+    const index = types.indexOf(desiredType);
 
-    if (desiredType !==null) {
-      for (let j = 0; j < types.length; j++ ) {
-        if(desiredType === types[j]) {
-          for (let i = 0; i < input.length; i++) {
-            if(input[i].type === types[j]) {
-             output.push(input[i]);
-            }
-          }
-          return output;
+    if (desiredType !== null) {
+      for (let i = 0; i < input.length; i++) {
+        if(input[i].type === types[index]) {
+         output.push(input[i]);
         }
       }
+      return output
     } else {
       return input;
     }
-
-
-    // if (desiredType !==null) {
-    //   console.log("hello. desiredType is not null");
-    //   types.forEach(function(type) {
-    //     if(desiredType === type) {
-    //       for (let i = 0; i < input.length; i++) {
-    //         if(input[i].type === type) {
-    //          output.push(input[i]);
-    //         }
-    //       }
-    //       return output;
-    //     }
-    //   });
-    // } else {
-    //   return input;
-    // }
-
-
-
-    // if(desiredType === "top") {
-    //   for (let i = 0; i < input.length; i++) {
-    //     if(input[i].type === "top") {
-    //      output.push(input[i]);
-    //     }
-    //   }
-    //   console.log(output);
-    //   return output;
-    // } else if(desiredType === "bottom") {
-    //   for (let i = 0; i < input.length; i++) {
-    //     if(input[i].type === "bottom") {
-    //      output.push(input[i]);
-    //     }
-    //   }
-    //   return output;
-    // } else if(desiredType === "one-piece") {
-    //   for (let i = 0; i < input.length; i++) {
-    //     if(input[i].type === "one-piece") {
-    //      output.push(input[i]);
-    //     }
-    //   }
-    //   return output;
-    // } else if(desiredType === "accessory") {
-    //   for (let i = 0; i < input.length; i++) {
-    //     if(input[i].type === "accessory") {
-    //      output.push(input[i]);
-    //     }
-    //   }
-    //   return output;
-    // } else if(desiredType === "dress") {
-    //   for (let i = 0; i < input.length; i++) {
-    //     if(input[i].type === "dress") {
-    //      output.push(input[i]);
-    //     }
-    //   }
-    //   return output;
-    // } else {
-    //   return input;
-    // }
   }
+  
 }
